a226e13aef263a5030452eaa0a5c743c85e31482
Enid aimee <82424233+Enidaimee@users.noreply.github.com>
2023-09-26 18:27:41 -0600

    Delete Unidad1Analisis/Factorial_Recursiva.py

--- Unidad1Analisis/Factorial_Recursiva.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    if( n== 0):
-        return 1
-    else:
-        return n * factorial(n-1)
-n = int(input("\nIngresa el Numero: "))
-print("El factorial de: " + str(n) + " Es: " + str (factorial(n)))
